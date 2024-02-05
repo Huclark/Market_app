1dd97a0a905b66cb4994638d596d372fb68cff93
Huclark <vhuclark@gmail.com>
2024-02-05 12:23:45 +0000

    Update: main.py

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
-from os import path
-from flask import Flask
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
+from market import create_app
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
+app = create_app()
 
-def create_app():
-    # name of app
-    app = Flask(__name__)
-    # create secret key
-    app.config['SECRET KEY'] = 'klajsfdksdfjaskfjhs'
-    # create database path
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + DB_NAME
-    # initialise the database
-    db.init_app(app)
-    # register all blueprints
-    # ******
-    # create database
-    create_database(app)
-    
-    return app
-
-def create_database(app):
-    """create the database
-    """
-    if not path.exists("market/" + DB_NAME):
-        with app.app_context():
-            db.create_all()
-    
+if __name__ == "__main__":
+    app.run(debug=True)
